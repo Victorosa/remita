b92c2467895d33e14527cc26d2179098f4cf0f03
victorosa <victorimadon@gmail.com>
2022-11-27 15:48:26 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -13,3 +13,10 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// echo "# remita" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Victorosa/remita.git
+// git push -u origin main
